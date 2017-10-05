de89656bce58b6cb241c85b83182ebc65c720e3e
Tianwei Zhang <tianwei_zhang@mckinsey.com>
2017-10-05 10:33:18 -0400

    updated check_na_rate

--- R/check_na_rate.R
+++ R/check_na_rate.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #' Check the NA value rate for each column in a dataframe
 #' @param x a data frame
 #' @return a data frame with variable name and the NA rate
@@ -7,14 +6,4 @@ check_na_rate=function(x){
   na_rate=scales::percent(colSums(is.na(x))/nrow(x))
   names(na_rate)=colnames(x)
   return(as.data.frame(na_rate))
-=======
-#' Check the NA value rate for each column in a dataframe
-#' @param x a data frame
-#' @return a data frame with variable name and the NA rate
-
-check_na_rate=function(x){
-  na_rate=scales::percent(colSums(is.na(x))/nrow(x))
-  names(na_rate)=colnames(x)
-  return(as.data.frame(na_rate))
->>>>>>> e1ead9dbd4db9f52b156cb51890cb762d6272174
 }
